1306d8da31b34d1714b7abde1c2e55295d2d3874
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2023-11-14 09:07:48 +0700

    Delete Shapes build folder

---
+++ build/CMakeFiles/ShapeParser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShapeParser.dir/main.cpp.o"
+  "CMakeFiles/ShapeParser.dir/main.cpp.o.d"
+  "ShapeParser"
+  "ShapeParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShapeParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
